dc98354c473aa11f845b5ef755827838c0ca0c57
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2023-08-27 23:35:47 +0100

    feat: Setups

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'OpskillPath',
+  description: 'Open Source Learning',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
