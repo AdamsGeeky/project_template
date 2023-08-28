b65f37cbaa52b0e3fae3bef29f67d8785a7a00f3
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2023-08-28 08:00:38 +0100

    feat: themes color setups

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +9,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['General Sans', ...fontFamily.sans],
       },
+      colors: {
+        primary: 'rgb(var(--primary) , <alpha-value>)',
+        divider: 'rgb(var(--divider) , <alpha-value>)',
+        content: 'rgb(var(--content) , <alpha-value>)',
+        'base-100': 'rgb(var(--base-100) , <alpha-value>)',
+        'base-200': 'rgb(var(--base-200) , <alpha-value>)',
+        'base-gray': 'rgb(var(--base-gray) , <alpha-value>)',
+        'primary-hover': 'rgb(var(--primary-hover) , <alpha-value>)',
+        'primary-disable': 'rgb(var(--primary-disable) , <alpha-value>)',
+      },
+      keyframes: {
+        spin: {
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },      
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
